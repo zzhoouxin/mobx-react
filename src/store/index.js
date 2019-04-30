473e88d14b9013a35118e726b0d5aa51f2795014
周鑫 <zhouxin@admin.local>
2019-04-30 16:44:59 +0800

    删除store引用

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,11 @@
-import PersonStore from "./PersonStore";
 import AppStore from "./AppStore";
 import ListStore from "./ListStore";
 
 const appStore = new AppStore();
-const personStore = new PersonStore();
 const listStore = new ListStore();
 
 const stores = {
   appStore,
-  personStore,
   listStore
 };
 /// 默认导出接口
